f05b3430eab7387d89dec4b87c4ecad10dc24111
satake <satake@onconnected.onmicrosoft.com>
2022-08-31 17:54:47 +0900

    commit

---
+++ pages/test-page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import Layout from '../components/Layout';
+
+const Test: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Hello next.js</h1>
+      <h1>test page</h1>
+    </Layout>
+  );
+};
+
+export default Test;
